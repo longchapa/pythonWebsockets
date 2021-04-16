9540016c532cf170ab96c9a9ce57a6399c61aa09
David Chaparro <david.chaparro@servicebots.net>
2021-04-16 07:45:57 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import asyncio
+import datetime
+import random
+import websockets
+
+async def time(websocket, path):
+    while True:
+        now = datetime.datetime.utcnow().isoformat() + 'Z'
+        await websocket.send(now)
+        await asyncio.sleep(random.random() * 3)
+
+start_server = websockets.serve(time, 'https://ec2-3-83-129-144.compute-1.amazonaws.com', 8080)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
