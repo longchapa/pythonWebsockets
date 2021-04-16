4e3b3b10063da634c0465ff0fc12736183d732a2
David Chaparro <david.chaparro@servicebots.net>
2021-04-16 09:36:34 -0500

    Test

--- src/main.py
+++ src/main.py
@@ -5,7 +5,9 @@ import datetime
 import random
 import websockets
 
-async def time(websocket, path):
+print("Hello World!!!")
+
+""" async def time(websocket, path):
     while True:
         now = datetime.datetime.utcnow().isoformat() + 'Z'
         await websocket.send(now)
@@ -14,4 +16,4 @@ async def time(websocket, path):
 start_server = websockets.serve(time, 'https://ec2-3-95-202-47.compute-1.amazonaws.com', 8080)
 
 asyncio.get_event_loop().run_until_complete(start_server)
-asyncio.get_event_loop().run_forever()
+asyncio.get_event_loop().run_forever() """
